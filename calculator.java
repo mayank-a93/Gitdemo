6fc36d44fad6edc03e88f0fe56469afc5375659a
mayank-a93 <1994mayankagarwal@gmail.com>
2016-01-11 12:42:49 +0530

    added multiply and mod

--- calculator.java
+++ calculator.java
@@ -11,6 +11,16 @@ public class Calc {
     {
         System.out.println("Difference = "+(a-b));
     }
+	
+	public void multiply(int a,int b)
+    {
+        System.out.println("Product = "+(a*b));
+    }
+	
+	public void divide(int a,int b)
+    {
+        System.out.println("Remainder = "+(a%b));
+    }
     
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
@@ -19,5 +29,7 @@ public class Calc {
         Calc c = new Calc();
         c.add(a,b);
         c.subtract(a,b);
+		c.multiply(a,b);
+		c.divide(a,b);
     }
 }
